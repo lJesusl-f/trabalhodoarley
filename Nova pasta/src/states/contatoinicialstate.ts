696d4f9f2e98f9d0a12c0f9e0f9ed17c455e7898
lJesusl-f <pedroenriquejacarei@gmail.com>
2026-03-26 11:19:50 -0300

    Update contatoinicialstate.ts

--- Nova pasta/src/states/contatoinicialstate.ts
+++ Nova pasta/src/states/contatoinicialstate.ts
@@ -1,9 +1,10 @@
-import { Lead } from "../models/Lead";
-import { LeadState } from "./LeadState";
-
 export class ContatoInicialState implements LeadState {
   avancar(lead: Lead) {
     lead.stage = "ENVIOU_PROPOSTA";
     lead.status = "EM_NEGOCIACAO";
+  }
+
+  desistir(lead: Lead) {
+    lead.status = "PERDIDO";
   }
 }
